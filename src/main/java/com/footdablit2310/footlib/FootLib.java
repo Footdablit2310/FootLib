6b4717b55d025d7eefc01bb67690755b8aa0eb84
Footdablit2310 <kolayparamita@gmail.com>
2026-07-18 13:24:14 +0530

    Removed Datagen and providers + FER due to API changes, they will be reimplemented at a later date.

--- src/main/java/com/footdablit2310/footlib/FootLib.java
+++ src/main/java/com/footdablit2310/footlib/FootLib.java
@@ -1,16 +1,10 @@
 package com.footdablit2310.footlib;
 
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.minecraft.world.item.CreativeModeTab;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import org.slf4j.Logger;
-
-import com.footdablit2310.footlib.api.common.commands.FootLibModListCommand;
-import com.footdablit2310.footlib.api.common.commands.FootLibVisualizeStructureCommand;
-import com.footdablit2310.footlib.network.FootLibNetwork;
 import com.mojang.logging.LogUtils;
 
 @Mod(FootLib.MOD_ID)
@@ -20,20 +14,6 @@ public final class FootLib {
     public static final Logger LOGGER = LogUtils.getLogger();
     public static DeferredHolder<CreativeModeTab, CreativeModeTab> FOOTLIB_TAB;
 
-    public FootLib(IEventBus modBus) {
-
-        // Register commands
-        net.neoforged.neoforge.common.NeoForge.EVENT_BUS.addListener(
-            FootLibModListCommand::register
-        );
-        net.neoforged.neoforge.common.NeoForge.EVENT_BUS.addListener(
-            FootLibVisualizeStructureCommand::register
-        );
-    }
-    @SubscribeEvent
-    public static void onRegisterPayloads(RegisterPayloadHandlersEvent event) {
-        FootLibNetwork.register(event);
-    }
-
+    public FootLib(IEventBus modBus) {}
 }
 
--- src/main/java/com/footdablit2310/footlib/api/FootLibServices.java
+++
@@ -1,7 +0,0 @@
-package com.footdablit2310.footlib.api;
-
-import com.footdablit2310.footlib.api.shared.foot_economy.EconomyAPI;
-
-public class FootLibServices {
-    public static EconomyAPI ECONOMY;
-}

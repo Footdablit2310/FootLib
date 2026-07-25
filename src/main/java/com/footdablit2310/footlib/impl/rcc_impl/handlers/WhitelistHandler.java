3301925fe19a18066b013a4fb33dac2f53ca089e
Footdablit2310 <kolayparamita@gmail.com>
2026-07-25 14:01:29 +0530

    Updated to make the API more user-friendly. Added proper SubCommand implementation. Added more accurate exception

--- src/main/java/com/footdablit2310/footlib/impl/rcc_impl/handlers/WhitelistHandler.java
+++ src/main/java/com/footdablit2310/footlib/impl/rcc_impl/handlers/WhitelistHandler.java
@@ -10,9 +10,14 @@ import net.minecraft.server.players.UserWhiteListEntry;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
 import org.slf4j.Logger;
 
+import java.io.InvalidObjectException;
+
 public class WhitelistHandler {
     public static final Logger LOGGER = FootLib.LOGGER;
-    public static RCCResponse handle(RCCCommand cmd) {
+    public static RCCResponse handle(RCCCommand cmd) throws InvalidObjectException {
+        if (!cmd.getSubcommand().isValid()) {
+            throw new InvalidObjectException("SubCommand is Invalid");
+        }
         String playerName = cmd.getData().get("player");
 
         MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
@@ -23,10 +28,17 @@ public class WhitelistHandler {
             if (player != null) {
                 GameProfile profile = player.getGameProfile();
                 UserWhiteList whitelist = server.getPlayerList().getWhiteList();
-                UserWhiteListEntry entry = new UserWhiteListEntry(profile);
-                whitelist.add(entry);
-
-                return new RCCResponse("success", "Player whitelisted", responseData);
+                if (cmd.getSubcommand().name().equalsIgnoreCase("ADD")) {
+                    UserWhiteListEntry entry = new UserWhiteListEntry(profile);
+                    whitelist.add(entry);
+                    return new RCCResponse("success", "Player added to whitelist", responseData);
+                } else if (cmd.getSubcommand().name().equalsIgnoreCase("REMOVE")) {
+                    if (whitelist.isWhiteListed(profile)) {
+                        whitelist.remove(profile);
+                        return new RCCResponse("success", "Player removed from whitelist", responseData);
+                    }
+                    return new RCCResponse("info", "Player was not in whitelist", responseData);
+                }
             } else {
                 LOGGER.warn("Player could not be found.");
                 return new RCCResponse("warning", "Player not found", responseData);
